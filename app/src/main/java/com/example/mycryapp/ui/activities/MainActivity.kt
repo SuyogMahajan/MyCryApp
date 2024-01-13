1137fb81735fd3945719dd4ddcc67b3f59ed40ea
SuyogMahajan <mahajansuyog9322@gmail.com>
2024-01-14 00:36:47 +0530

    implemented auto refresh

--- app/src/main/java/com/example/mycryapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mycryapp/ui/activities/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mycryapp.ui.activities
 
 import CoinDataViewModel
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.DragEvent
+import android.view.View
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,11 +20,16 @@ import com.example.mycryapp.utils.Result
 import com.example.mycryapp.data.models.MarketListData
 import com.example.mycryapp.databinding.ActivityMainBinding
 import com.focus.cryptotracker.ui.adapters.CoinItemAdapter
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: CoinDataViewModel
     private lateinit var binding: ActivityMainBinding
     private lateinit var coinItemAdapter:CoinItemAdapter
+    private lateinit var mainHandler: Handler
     private var loading = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +37,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+         mainHandler = Handler(Looper.getMainLooper())
+
         coinItemAdapter = CoinItemAdapter(this)
 
         val onRvItemClick = object :CoinItemAdapter.onItemClickInterface{
             override fun onItemClick(coin: Coin) {
-
+                // this will work if any item is clicked
             }
         }
 
@@ -45,9 +58,27 @@ class MainActivity : AppCompatActivity() {
             refreshData()
         }
 
+        binding.floatingButton.setOnClickListener {
+            binding.CoinRefreshLayout.isRefreshing = true;
+            refreshData()
+        }
+
+        mainHandler.post(object :Runnable {
+            override fun run() {
+                refreshData()
+                mainHandler.postDelayed(this,180000)
+            }
+        })
+
     }
 
     private fun refreshData(){
+        binding.onlineStatus.visibility = View.GONE
+        binding.floatingButton.visibility = View.GONE
+
+            val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm")
+        val curr = formatter.format(Date())
+        binding.lastRefreshed.text = curr.toString()
         viewModel.getLive().observe(this,object:Observer<Result<MarketLiveData>>{
 
             override fun onChanged(value: Result<MarketLiveData>) {
@@ -59,6 +90,8 @@ class MainActivity : AppCompatActivity() {
                     }
                     is Result.Error -> {
                         loading = false
+                        binding.onlineStatus.visibility = View.VISIBLE
+                        binding.floatingButton.visibility = View.VISIBLE
                         Log.d("Message!!",value.exception.message!!)
                     }
                     else -> {
@@ -79,7 +112,9 @@ class MainActivity : AppCompatActivity() {
                     }
                     is Result.Error -> {
                         loading = false
-                        Log.d("Message!!",value.exception.message!!)
+                        binding.onlineStatus.visibility = View.VISIBLE
+                        binding.floatingButton.visibility = View.VISIBLE
+                            Log.d("Message!!", value.exception.message!!)
                     }
                     else -> {
                         loading = true
